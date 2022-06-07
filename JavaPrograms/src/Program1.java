4e0803b8b959e73b01afecc4748bc75fbf826de9
Vinayak <Vinayak@192.168.0.6>
2022-06-07 19:15:56 +0530

    This is my first commit

---
+++ JavaPrograms/src/Program1.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Program1 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		String name="Kartik Ganiger";
+		String [] words=name.split(" ");
+		String reverseString="";
+		for(String word:words)
+		{
+			int count=word.length();
+			String reversed="";
+			for(int i=count-1; i>=0; i--)
+			{
+				reversed=reversed+word.charAt(i);
+			}
+			reverseString=reverseString+reversed+" ";
+			
+		}
+		
+		System.out.println(reverseString);
+			
+		
+		
+	}
+
+}
